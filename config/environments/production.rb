b1d80c7c3a0b23070008d87020e02fd0bcd26950
rissyrussell <rissyrussell@gmail.com>
2014-08-11 11:02:51 -0700

    added _id to secret access key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ config.paperclip_defaults = {
   :s3_credentials => {
     :bucket => ENV['AWS_BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY_ID']
   }
 }
 
