3575a96c263e7dfa7fca5a419a414948958230fe
rissyrussell <rissyrussell@gmail.com>
2014-08-11 10:58:23 -0700

    update pin.rb again

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,8 +4,6 @@ class Pin < ActiveRecord::Base
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
 	:storage => :s3,
             :bucket  => ENV['AWS_BUCKET']
-    validates :image, presence: true
-    validates :description, presence: true
 
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
