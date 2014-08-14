cff2866de75986b5fb85e3ccfb996ac546f17822
rissyrussell <rissyrussell@gmail.com>
2014-08-14 12:45:59 -0700

    moved aws gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,16 @@ Rails.application.configure do
 
 #required for heroku
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+Paperclip.options[:command_path] = "/usr/local/bin/"
+
+config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY_ID']
+    }
+}
+
+
 end
